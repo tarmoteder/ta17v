89e40489137cadf157d7463c2436e003fc7c2118
Tarmo Teder <Tarmo.Teder@tptlive.ee>
2018-09-24 15:36:20 +0300

    First commit, 24.09.2018

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+$greeting = "Privet!";
+$number = 2;
+$number = 4.12;
+$bool = false;
+
+$arr = array ("Tarmo", $greeting, 3.14);
+
+echo "$greeting $arr[0] <br>";
+echo '$greeting ljudi';
+
+?>
